ee9ce7ed7f25fea0791696f4bb3f5c6d73bb27e3
Federico Pigozzi <federicopigozzi@MacBook-Air-di-Federico.local>
2019-03-27 18:25:59 +0100

    implemented radix sort

--- Retrieving_Data_and_Sorting/sorting_and_searching.cc
+++ Retrieving_Data_and_Sorting/sorting_and_searching.cc
@@ -94,98 +94,140 @@ void heapsort(T* A, const std::size_t n) {
     A = std::move(h.data);
 }
 
-// counting sort. Since our implementation default-initializes C to 0,
-// we can get rid of the template
+// counting sort. Generalizes to any domain bounded by k1 as the minimum and k2 as the maximum
 
 template<class T>
-void counting_sort(T* A, T* B, const std::size_t k, const std::size_t n) {
-    int* C{new int[k + 1]};
-    for (std::size_t j=0; j <= k; ++j) {
+void counting_sort(T* A, T* B, const std::size_t k1, const std::size_t k2, const std::size_t n) {
+    std::size_t k{k2 - k1 + 1};
+    int* C{new int[k]};
+    for (std::size_t j=0; j < k; ++j) {
         C[j] = 0;
     }
     for (std::size_t i=0; i < n; ++i) {
-        ++C[A[i]];
+        ++C[A[i] - k1];
     }
-    for (std::size_t j=1; j <= k; ++j) {
+    for (std::size_t j=1; j < k; ++j) {
         C[j] += C[j - 1];
     }
     for (std::size_t i=n; i >= 1; --i) {
-        int v{A[i-1]};    // we use i-1 because otherwise the condition is always true for std::size_t
-        B[C[v] - 1] = v;
-        --C[v];
+        T v{A[i-1]};    // we use i-1 because otherwise the condition is always true for std::size_t
+        B[C[v - k1] - 1] = v;
+        --C[v - k1];
     }
     delete[] C;
 }
-/*
-template<class T, class Args...>
-struct Sorter()
-*/
+
 // radix sort
-/*
-template<class T, class Sort>  // Sort is the template for the sorting procedure to be used
-void radix_sort(T* A, const int i) {  // i is the max number of digits
-    Sort sorter{};
-    for (int j=0; j < i; ++j) {
 
-
-    }
-}
-*/
-// bucket sort
-/*
 template<class T>
-struct Bucket {
+void radix_sort(T* A, const int d, const std::size_t n) {  // d is the max number of digits
+    // C is the counter array, B will store intermediate outputs
+    int* C{new int[10]};
+    T* B{new T[n]};
+    // for each digit j from the least significant to the most significant
+    for (int j=1; j < pow(10, d); j*=10) {
+        // perform counting sort according to digit j
+        for (std::size_t i=0; i < 10; ++i) {
+            C[i] = 0;
+        }
+        for (std::size_t i=0; i < n; ++i) {
+            ++C[(A[i] / j) % 10];
+        }
+        for (std::size_t i=1; i < 10; ++i) {
+            C[i] += C[i - 1];
+        }
+        for (std::size_t i=n; i >= 1; --i) {
+            T v{A[i-1]};
+            B[C[(A[i-1] / j) % 10] - 1] = v;
+            --C[(A[i-1] / j) % 10];
+        }
+        // save intermdiate result into A
+        for (std::size_t i=0; i < n; ++i) {
+            A[i] = B[i];
+        }
+    }
+    delete[] B;
+    delete[] C;
+}
+
+// bucket sort
+
+template<class T>
+class Bucket {
+    struct BucketNode {
+        T data;
+        BucketNode* next;
+        BucketNode(T& val) : data{val}, next{nullptr} {}
+    };
+
+    BucketNode* current;
+    BucketNode* head;
+
+  public:
     std::size_t size;
-    T* data;
-    Bucket(const std::size_t dim) : size{0}, data{new T[dim]} {}
-    void append(const T& value) noexcept {
-        data[size] = value;
+    Bucket() : size{}, current{nullptr}, head{nullptr} {}
+    void append(const T& value) {
+        BucketNode node{value};
+        head->next = node;
+        head = node;
         ++size;
     }
-    //T& operator[]() const noexcept {++size; return data[size-1];}
+    T get() {
+        T ans{current->data};
+        current = current->next;
+        return ans;
+    }
 };
 
-
-template<class T, class Sort>
-void bucket_sort(T* A, const std::size_t n, ... args) {
-    Sort sorter{};
-    Bucket<T>* B{new Bucket<T>[n]{n}};
+/*
+template<class T, class Sort, class Args>
+void bucket_sort(T* A, const std::size_t n, Args ... args) {
+    //Sort sorter{};
+    Bucket<T>* B{new Bucket<T>[n]{}};
     for (std::size_t i=0; i < n; ++i) {
         B[floor(A[i] / n) + 1].append(A[i]);
     }
     std::size_t i{0};
     for (std::size_t j=0; j < n; ++j) {
-        Bucket<T> curr{B[j]};
+        Bucket<T> curr = B[j];
         for (std::size_t k=0; k < curr.size; ++k) {
-            A[i] = curr.data[k];
+            A[i] = curr.get();
             ++i;
         }
-        sorter(curr, args...);
-        curr.size = 0;
-        for (std::size_t i=0; i < curr.size; ++i) A[j] = curr.data[i];
+        sorter(curr, args..., );
     }
+    delete[] B;
 }
 */
+template<class T>
+int select(T*A, int i, int l, int r, int threshold=0);
 
 template<class T>
 int select_pivot(T* A, int l, int r) {
-    for (int i=0; i < (r - l)/5; ++i) {
+    for (int i=0; i <= (r - l)/5; ++i) {
         int a{5*i + l + 4};
-        insertion_sort(A, 5*i + l, (a > r) ? r : a);
+        int k{5*i + l};
+        insertion_sort(A + k, (a > r) ? r - k + 1: a - k + 1);
     }
-    for (int i=0; i < (r - l)/5; ++i) {
+    for (int i=0; i <= (r - l)/5; ++i) {
         int a{5*i + l + 2};
         swap(A, l+i, (a > r) ? a : r);
     }
-    return select(A, ((r - l)/5 + 1)/2, l, l + (r - l)/5);
+    for (int i=l; i < l + (r - l)/5; ++i) std::cout << A[i] << std::endl;
+    auto ans = select(A, ((r - l)/5 + 1)/2, l, l + (r - l)/5);
+    //std::cout << ans;
+    //abort();
+    return ans;
 }
 
 template<class T>
-int select(T* A, int i, int l, int r, int threshold=10) {
+int select(T* A, int i, int l, int r, int threshold) {
     if ((r - l) <= threshold) {
         insertion_sort(A, r - l);
-        return A[i];
+        //for (int i=l; i < r; ++i) std::cout << A[i] << std::endl;
+        return i;
     }
+    //std::cout << r << " " << l << std::endl;
     int j{select_pivot(A, l, r)};
     int k{partition(A, l, r, j)};
     if (i == k) {
@@ -197,19 +239,24 @@ int select(T* A, int i, int l, int r, int threshold=10) {
     return select(A, i, k + 1, r);
 }
 
-int main() {/*
-    int test[10] = {13, 5, 7, 2, 1, 4, 1, 11, 6, 1};
-    int test2[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    int (*choose_pivot) (const int, const int);   // to be passed to quicksort in order to select the pivot
-    choose_pivot = [] (const int l, const int r) -> int {(void)r; return l;};
-    quicksort(test, 0, 10, choose_pivot);
-
+int main() {
+    //int test[10] = {13, 5, 7, 2, 1, 4, 1, 11, 6, 1};
+    //int test[24] = {13, 5, 7, 2, -7, 4, 1, 11, 6, 0, 10, 15, -1, 8, -2, 3, 14, 12, -9, -5, 9, -6, 16, -4};
+    int test[4] = {3475, 2475, 3471, 1733};
+    radix_sort(test, 4, 4);
+    //int test2[10];// = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    //int (*choose_pivot) (const int, const int);   // to be passed to quicksort in order to select the pivot
+    //choose_pivot = [] (const int l, const int r) -> int {(void)r; return l;};
+    //quicksort(test, 0, 10, choose_pivot);
+//counting_sort(test, test2, 2, 14, 10);
+    //int ans = select(test, 0, 0, 23);
+    //std::cout << ans;
     //int test2[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     //void (*sort) (int*, 10);
     //sort = &insertion_sort;
     //bucket_sort<int>(test, 10, 10);
-    for (std::size_t i=0; i < 10; ++i) std::cout << test[i] << " " ;//std::endl;
-   /*/
+    for (std::size_t i=0; i < 4; ++i) std::cout << test[i] << std::endl;
+   /*
     std::cout << "TESTING THE IMPLEMENTATION OF INSERTION_SORT IN THE WORST CASE:" << std::endl;
     for (std::size_t dim : {100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000}) {
         int test[dim];
@@ -291,7 +338,7 @@ int main() {/*
         // print result
         std::cout << "Size: " << dim << std::endl;
         std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << std::endl;
-    }
+    }*/
 
     return 0;
 }
