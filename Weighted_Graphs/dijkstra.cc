27763902da66ecf4a1aa33fe8768708ecbee68f5
Federico Pigozzi <federicopigozzi@Air-di-Federico.homenet.telecomitalia.it>
2019-06-29 22:55:54 +0200

    fixes

--- Weighted_Graphs/dijkstra.cc
+++ Weighted_Graphs/dijkstra.cc
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <chrono>
-#include <graph_utilities.h>
-#include <heap.h>
+
+#include "graph_utilities.h"
+#include "heap.h"
+
+#define N 6  // number of vertices of the graph
 
 
 /**
-  * Run Dijkstra's SSSP algorithm on a graph, given the an array of Vertex instances, their number and a reference to the source vertex.
-  * The representation of the graph is given by an adjacency matrix, since the adjacency list representation turned out to be somewhat complex
-  * to program without the STL. The complexity is Theta(|V|^2 + |E|) with an array-based implementation of the queue data structure (the class
-  * 'Queue' coming from the graph_utilities.h header), while O(log|V| * (|V| + |E|)) with a BinaryHeap data sructure, which had been implemented in the
-  * heap.h header for a previous assignment. Notice the template defines the queue data structure to use.
+  * Run Dijkstra's SSSP algorithm on a graph, given an array of Vertex instances 'V', their number 'n' and a reference to the source vertex 's'.
+  * The representation of the graph is given by an adjacency matrix. Notice the template defines the queue data structure to use, which will be
+  * (at least in our tests) either an array-based implementation of the queue data structure (the class 'Queue' coming from the graph_utilities.h
+  * header file), or a BinaryHeap data structure, implemented in the heap.h header for a previous assignment.
   */
 template<class Q>
-void dijkstra(int** graph, Vertex V[], const std::size_t n, Vertex& s) {
+void dijkstra(int graph[][N], Vertex V[], const std::size_t n, Vertex& s) {
     s.d = 0;  // set source distance to 0
     Q q{V, n};  // build queue from the vertices
+    // iterate until while there are still nodes to finalize
     while (!q.is_empty()) {
         // while the queue is not empty, extract the minimum and mark it as no longer in queue
         Vertex& u = V[q.extract_min()];
         u.on_queue = false;
         // then iterate over the neighbors (a row in the adjacency matrix) and perform the relaxation step, if necessary
         for (std::size_t i=0; i < n; ++i) {
-            int w = graph[u.index][i];
-            Vertex& v = V[i];
-            // -1 stands for no edge. Notice this is not an issue, since Dijkstra's algorithm is not intended for graphs with negative weights
+            int w = graph[u.index][i];  // the weight of the edge
+            Vertex& v = V[i];  // the neighbor
+            // -1 stands for no edge. Notice this is not a constraint, since Dijkstra's algorithm is not intended for graphs with negative weights
             if (w != -1 && v.on_queue == true) {  // only neighbors which are still in the queue
                 // perform the relaxation step of Dijkstra's algorithm. Check if the candidate distance of v is greater than
                 // its parent's distance plus the weight of the edge
@@ -40,44 +43,43 @@ void dijkstra(int** graph, Vertex V[], const std::size_t n, Vertex& s) {
 
 int main() {
     // initialize list of vertices and adjacency matrix, which will be a pointer to pointer
-    Vertex* vertices = new Vertex[6];
-    int** graph = new int*[6];
-    for (int i = 0; i < 6; ++i) {
-        graph[i] = new int[6]{-1};
-        for (int j=0; j < 6; ++j) graph[i][j] = -1;  // fill the matrix with -1, our default for no edge
+    Vertex* vertices = new Vertex[N];
+    for (int i = 0; i < N; ++i) {
         vertices[i] = Vertex{i};
     }
-    // define first custom graph. This graph is the same illustrated in lecture
-    graph[0][1] = 1;
-    graph[0][2] = 5;
-    graph[1][5] = 15;
-    graph[2][3] = 2;
-    graph[3][4] = 1;
-    graph[4][5] = 3;
+    // define custom graph. This graph is the same illustrated in lecture
+    int graph[N][N] = {{-1, 1, 5, -1, -1, -1}, {-1, -1, -1, -1, -1, 15}, {-1, -1, -1, 2, -1, -1}, {-1, -1, -1, -1, 1, -1}, {-1, -1, -1, -1, -1, 3}, {-1, -1, -1, -1, -1, 3}};
+    // print the adjacency matrix
+    //for (int i=0; i < 6; ++i) {
+    //    for (int j=0; j < 6; ++j) std::cout << graph[i][j] << " ";
+    //    std::cout << std::endl;
+    //}
     // test first with BinaryHeap
-    std::cout << "Tests with graph number one" << std::endl;
+    std::cout << "Tests with lecture graph" << std::endl;
     auto start = std::chrono::high_resolution_clock::now();
-    dijkstra<BinaryHeap<Vertex, CompareVertex>>(graph, vertices, 6, vertices[0]);
+    dijkstra<BinaryHeap<Vertex, CompareVertex>>(graph, vertices, N, vertices[0]);
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << "BinaryHeap implementation: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << std::endl;
+    // check result
+    for (int i=0; i < 6; ++i) {
+        std::cout << "node number: " << vertices[i].index << " has distance: " << vertices[i].d << std::endl;
+    }
     // reset vertices' members
     for (int i=0; i < 6; ++i) {
+        vertices[i].on_queue = true;
         vertices[i].pred = -1;
         vertices[i].d = INT_MAX;
     }
     // test with Queue
     start = std::chrono::high_resolution_clock::now();
-    dijkstra<Queue>(graph, vertices, 6, vertices[0]);
+    dijkstra<Queue>(graph, vertices, N, vertices[0]);
     end = std::chrono::high_resolution_clock::now();
     std::cout << "Array implementation: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << std::endl;
-    //for (int i=0; i < 6; ++i) {
-    //    std::cout << "node number: " << vertices[i].index << " has distance: " << vertices[i].d << std::endl;
-    //}
-    // deallocate
-    for (int i = 0; i < 6; ++i) {
-        delete[] graph[i];
+    // check result
+    for (int i=0; i < 6; ++i) {
+        std::cout << "node number: " << vertices[i].index << " has distance: " << vertices[i].d << std::endl;
     }
-    delete[] graph;
+    // deallocate
     delete[] vertices;
     return 0;
 }
--- Weighted_Graphs/graph_utilities.h
+++ Weighted_Graphs/graph_utilities.h
@@ -1,16 +1,18 @@
-#include <iostream>
-#include <math.h> // for INT_MAX
+#ifndef __GRAPH_UTIL__
+#define __GRAPH_UTIL__
 
 /**
   * This header file includes structs and classes used by the Dijkstra's algorithm implementation of dijkstra.cc
   */
 
+#include <iostream>
+#include <math.h> // for INT_MAX
+
 
 /**
   * Implementation of the vertex data structure, to be used as part of a graph. The members have been modeled in function
-  * of what needs to be done in Dijkstra's algorithm. Notice the List member 'neighbors' allows us to implement an
-  * adjacency list representation of a graph. Without loss of generality, we can use integers from 0 to label vertices,
-  * since the user can always define an index (like an hashtable, or a tree) to map from the original labels to the integers.
+  * of what needs to be done in Dijkstra's algorithm. Without loss of generality, we can use integers from 0 to label vertices,
+  * since the user can always define an index (like an hashtable) to map from the original labels to the integers.
   * Notice the integer -1 has been used as a proxy for NULL in the 'pred' member.
   */
 
@@ -18,20 +20,12 @@ struct Vertex {
     int index;  // index of the vertex
     int d;  // distance
     int pred;  // predecessor in the shortest-paths tree
-    //int* neighbors;  // adjacency list of the neighbors
     bool on_queue;  // if the vertex is still in the queue
-    /**
-      * Default constructor
-      */
+    // Default constructor
     Vertex() = default;
-    /**
-      * Constructs a node from a given index. Notice all the nodes are assumed to be on the queue at the beginning.
-      * Even if this might sound like a weird assumption, remember that this implementation is Dijkstra-focused.
-      */
+    // Constructs a node from a given index 'idx'. Notice all the nodes are assumed to be on the queue at the beginning
     Vertex(const int idx) : index{idx}, d{INT_MAX}, pred{-1}, on_queue{true} {}
-    /**
-      * Default destructor
-      */
+    // Default destructor
     ~Vertex() = default;
 };
 
@@ -42,7 +36,7 @@ struct Vertex {
   */
 struct CompareVertex {
     CompareVertex() = default;
-    bool operator()(const Vertex& a, const Vertex&b) const noexcept {
+    bool operator()(const Vertex& a, const Vertex& b) const noexcept {
         return a.d < b.d;
     }
     bool operator()(const Vertex& vertex, const int value) const noexcept {
@@ -55,10 +49,9 @@ struct CompareVertex {
   * Queue data structure implementation using arrays. It keeps an internal array of data,
   * which can be manipulated using the extract_min and operator[] functions. This
   * is intended to store the nodes during the computation of Dijkstra's algorithm, so
-  * no insertions are possible after the constructor has been called. Since the queue has
-  * to keep distances, which are integers, templating is not necessary
+  * no insertions are possible after the constructor has been called. Since the queue
+  * keeps distances, which are integers, templating is not necessary
   */
-
 class Queue {
     std::size_t size;  // the index for the next element after the last one
     std::size_t free_slots;  // the number of slots available, necessary for reallocation
@@ -66,28 +59,20 @@ class Queue {
     int* data;  // array of data to store
 
   public:
-    /**
-      * Constructor, builds a queue by copying the distance associate to each Vertex into 'data', whose size is defined by n.
-      * 'array' is an adjency list representation of a graph. Equivalent to BUILD_QUEUE. Since we have to copy once each
-      * element, the complexity is Theta(n)
-      */
+    // Constructor, builds a queue by copying the distance associated to each Vertex into 'data', whose size is defined by 'n'.
+    // 'array' is an adjency list representation of a graph. Equivalent to BUILD_QUEUE.
     Queue(Vertex graph[], const std::size_t n) : size{n}, free_slots{0}, num{n}, data{new int[size]} {
         // copy the array elements one by one
         for (std::size_t i=0; i < n; ++i) {
             data[i] = graph[i].d;
         }
     }
-    /**
-      * Tests whether the queue does not contain any element
-      */
+    // Tests whether the queue does not contain any element
     bool is_empty() const noexcept {
         return num == 0;
     }
-    /**
-      * Finds the minimum of the array and returns it, effectively washing it
-      * away from the data. Since we have to iterate over all the elements twice,
-      * the complexity is Theta(2n) = Theta(n)
-      */
+    // Finds the minimum of the array and returns it, effectively washing it
+    // away from the data
     std::size_t extract_min() noexcept {
         // trivial algorithm for finding the minimum of an array. At the end,
         // 'minimum' will store the minimum of the array and 'index' its index
@@ -105,16 +90,15 @@ class Queue {
         ++free_slots;
         return index;
     }
-    /**
-      * Overload of operator[], queue[i] returns the element in position i of 'data'.
-      * Notice that returning by reference allows us to update the distance as if it were
-      * UPDATE_DISTANCE. Complexity is Theta(1)
-      */
+    // Update the value of data[i] to 'value', just like
+    // UPDATE_DISTANCE
     void decrease(const std::size_t i, const int value) {
         data[i] = value;
     }
-    /**
-      * Destructor
-      */
-    ~Queue() {delete[] data;}
+    // Destructor
+    ~Queue() {
+        delete[] data;
+    }
 };
+
+#endif  // __GRAPH_UTIL__
