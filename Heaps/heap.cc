66168509f6e0402d04b3f984a0307802614770ab
Federico Pigozzi <federicopigozzi@Air-di-Federico.homenet.telecomitalia.it>
2019-06-29 19:12:01 +0200

    details and doc

--- Heaps/heap.cc
+++ Heaps/heap.cc
@@ -1,31 +1,55 @@
 #include <iostream>
 #include <functional>
 #include <chrono>
-#include "heap.h"
+//#include <sys/time.h>
 
+#include "heap.h"
+// maximum value for the heap elements
 #define MAX_VALUE 25
 
+/**
+  * This function returns the number of second elapsed since
+  * the start of the Unix epoch on 1 January, 1970
+  *//*
+double seconds() {
+    struct timeval tmp;
+    double sec;
+    gettimeofday(&tmp, (struct timezone*)0);
+    sec = tmp.tv_sec * 1000000.0 + tmp.tv_usec;
+    return sec;
+}*/
+
 int main() {
+    // in the following, tests will be made using the functionalities of the
+    // <chrono> header. I am aware that this is a break of the no-STL rule,
+    // but the clock used by C++ is the highest-resolution clock in the world,
+    // and the functions of the <time.h> header provided sometime bogus results
+
     std::cout << "TESTING THE IMPLEMENTATION OF HEAPIFY:" << std::endl;
+    // random seed
     srand(0);
     // loop over the set of dimensions to investigate
-    for (std::size_t dim : {10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000}) {
+    for (std::size_t dim : {1000, 5000, 10000, 50000, 100000, 500000, 1000000}) {
         int test[dim];
         // initialize array of random values
         for (std::size_t i=0; i < dim; ++i) {
             test[i] = rand()%(2*MAX_VALUE)-MAX_VALUE;
         }
-        {//new scope to make sure the destructor of BinaryHeap gets called before the next iteration
-        BinaryHeap<int,std::greater<int>> h{test, dim};
-        h[0] = 0.0;    // break heap property
-        // measure time
-        auto start = std::chrono::high_resolution_clock::now();
-        h.heapify(0);
-        auto end = std::chrono::high_resolution_clock::now();
-        // print result
-        std::cout << "Size: " << dim << std::endl;
-        std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << std::endl;
-      }
+        { //new scope to make sure the destructor of BinaryHeap gets called before the next iteration,
+          // to avoid subtle memory leaks
+            // build a max-heap of int, having elements in 'test' and dimensions 'dim'
+            BinaryHeap<int, std::greater<int>> h{test, dim};
+            h[0] = 0.0;    // break heap property, so that a call to heapify on the root makes sense
+            // measure time
+            //double start = seconds();
+            auto start = std::chrono::high_resolution_clock::now();
+            h.heapify(0);
+            auto end = std::chrono::high_resolution_clock::now();
+            //double end = seconds();
+            // print result
+            std::cout << "Size: " << dim << std::endl;
+            std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << std::endl;
+        }
     }
     return 0;
 }
--- Heaps/heap.h
+++ Heaps/heap.h
@@ -1,111 +1,155 @@
+#ifndef __HEAP__
+#define __HEAP__
+
 #include <iostream>
-#include <stdexcept>
+#include <math.h>
 
 // some useful macros to define heap invariants
 #define LEFT(x) 2*x + 1
 #define RIGHT(x) 2*x + 2
-#define PARENT(x) (x - 1) / 2
+#define PARENT(x) floor((x - 1) / 2)
 #define GET_ROOT() 0
 #define IS_ROOT(x) x == 0
 
+/**
+  * The Heap class is templated on the type of the data to store and the type of comparison
+  * to define the heap propriety. It implements all the heap algorithms touched in lecture.
+  * In the 'heap.cc' script is possible to run a test to benchmark the execution time of HEAPIFY,
+  * and, as expected, it turns out to be O(logn), where n is the number of nodes of the heap.
+  */
 template<class T, class Comp>
 struct BinaryHeap {
     // data
-    std::size_t size;
-    std::size_t free_slots;
-    T* data;    // array of elements
-    Comp compare;
-    // constructor (BUILD_HEAP procedure). Notice the call to std::move allows us to implement heapsort in place
+    std::size_t size;  // size of the heap
+    std::size_t free_slots;  // free slots in the array representation
+    T* data;  // array of elements
+    Comp compare;  // comparison operator
+    // constructor (BUILD_HEAP procedure). Notice the call to std::move allows us to implement heapsort in place.
+    // Takes the 'array' to take elements from, its size 'n', and a boolean flag 'inplace'
     BinaryHeap(T* array, const std::size_t n, const bool inplace=false) : size{n}, free_slots{0}, data{new T[size]}, compare{} {
         if (inplace) {
-            data = std::move(array);
+            // repositioning the pointer allows us to work with one copy of the same array.
+            // Notice that, as a result, any change to the heap array will be reflected on
+            // the outer array as well
+            data = array;
         }
+        // if build is not in-place, copy elements one by one from 'array'
         else {
             for (std::size_t i=0; i < size; ++i) {
                 data[i] = array[i];
             }
         }
-        for (int i=PARENT(size-1); i >= 0; --i) {  //int because the condition is always true for std::size_t
+        // call HEAPIFY bottom-up
+        for (int i=PARENT(size-1); i >= 0; --i) {  // int because the condition is always true for std::size_t
             heapify(i);
         }
     }
-    // overload of operator[]
-    T& operator[](const std::size_t i) noexcept {return data[i];}
+    // overload of operator[], returns by reference
+    T& operator[](const std::size_t i) noexcept {
+        return data[i];
+    }
+    bool is_empty() const noexcept {
+        return size == 0;
+    }
     // HEAP_MINIMUM procedure to remove the minimum, or whatever optimum
-    T get_opt() noexcept;
-    // HEAP_DECREASE_KEY procedure. Throws std::invalid_argument if value is greater than or equal to data[i]
-    void decrease(std::size_t i, const T& value);
-    // HEAP_INSERT procedure
-    void insert(const T& value);
+    int extract_min() noexcept {
+        // extract the root and replace it with the rightmost leaf
+        int ans = data[0].index;
+        data[0] = data[size - 1];
+        // update size and free space
+        --size;
+        ++free_slots;
+        // call heapify on the root
+        heapify(GET_ROOT());
+        return ans;
+    }
+    // HEAP_DECREASE_KEY procedure; decrease the element H[i] to 'value'
+    void decrease(std::size_t i, const T& value) {
+        // if the value does not imply a decrease, abort the program
+        if (compare(data[i], value)) {
+            std::cout << "value is not smaller than H[i]" << std::endl;
+            abort();
+        }
+        data[i] = value;
+        // push the problem one level up to the root
+        bubble_up(i);
+    }
+    // same as above, but using an integer as new value. Necessary for binary heap-based implementation of Dijkstra's algorithm
+    void decrease(std::size_t i, const int value);
+    // HEAP_INSERT procedure. In the pseudocode provided, the last node is initialized to the maximum possible
+    // value with respect to the comparison operator, before being updated to 'value'. Here we decided to skip
+    // this part, since its implementation would have required the creation of an ad-hoc class overriding the
+    // usual comparison operators in such a way to behave as a maximum, with no gain in terms of correctness
+    /*void insert(const T& value) {
+        check_and_alloc();
+        decrease(size, value);
+        //data[size] = value;
+        //bubble_up(size);
+        ++size;
+        --free_slots;
+    }*/
     // HEAPIFY routine, iterative version
-    void heapify(std::size_t i) noexcept;
+    void heapify(std::size_t i) noexcept {
+          std::size_t m{i};
+          // iterate as long as the current node doesn't satisfy the heap propriety, else break
+          while (true) {
+                // for the left and right children, if it is a valid node and the key is smaller or equal
+                // (borrowing notation from a min-heap) than the current node, let m be equal to it
+                if (is_valid_node(LEFT(i)) && compare(data[LEFT(i)], data[m])) {
+                    m = LEFT(i);
+                }
+                if (is_valid_node(RIGHT(i)) && compare(data[RIGHT(i)], data[m])) {
+                    m = RIGHT(i);
+                }
+                // if i is different from m, then one of the two children breaks the heap propriety
+                // and a swap is necessary to push the problem one level down
+                if (i != m) {
+                    swap(i, m);
+                    i = m;
+                }
+                // if such a situation did not occur, by induction the heap propriety must hold heap-wise
+                // and we can stop
+                else break;
+           }
+    }
     // swap helper function
     void swap(const std::size_t i, const std::size_t m) noexcept {
         T temp{data[m]};
         data[m] = data[i];
-        data[i] = std::move(temp);
+        // this could have been made more efficient using move semantics,
+        // but we will stick to the assignment and avoid the STL as much as possible
+        data[i] = temp;
     }
     // utility function to check whether an index corresponds to a valid node
     bool is_valid_node(const std::size_t i) const noexcept {return size-1 >= i;}
     // utility function to manage reallocation. For simplicity, it doubles the array each time
-    void check_and_alloc() {
+    /*void check_and_alloc() {
         if (free_slots != 0) return;
         T* new_data{new T[size * 2]};
         for (std::size_t i=0; i < size; ++i) new_data[i] = data[i];
         data = std::move(new_data);
         free_slots = size;
-    }
+    }*/
     // bubble-up helper function
-    void bubble_up(std::size_t i) noexcept;
-
+    void bubble_up(std::size_t i) noexcept {
+        // if the node is not the root and violates the heap propriety with respect to
+        // its parent, swap them and move up
+        while (!(IS_ROOT(i)) && compare(data[i], data[PARENT(i)])) {
+            swap(i, PARENT(i));
+            i = PARENT(i);
+        }
+    }
+    // destructor
+    ~BinaryHeap() {
+        delete[] data;
+    }
 };
 
 template<class T, class Comp>
-void BinaryHeap<T,Comp>::heapify(std::size_t i) noexcept {
-    std::size_t m{i};
-    // iterate as long as the current node doesn't satisfy the heap propriety, else break
-    while (true) {
-        for (auto j : {LEFT(i), RIGHT(i)}) {
-            if (is_valid_node(j) && compare(data[j], data[m])) m = j;
-        }
-        if (i != m) {
-            swap(i, m);
-            i = m;
-        }
-        else break;
-    }
-}
-
-template<class T, class Comp>
-void BinaryHeap<T,Comp>::bubble_up(std::size_t i) noexcept {
-    while (!(IS_ROOT(i)) && compare(data[i], data[PARENT(i)])) {
-        swap(i, PARENT(i));
-        i = PARENT(i);
-    }
-}
-
-template<class T, class Comp>
-T BinaryHeap<T,Comp>::get_opt() noexcept {
-    T ans{data[0]};
-    data[0] = data[size - 1];
-    --size;
-    ++free_slots;
-    heapify(GET_ROOT());
-    return ans;
-}
-
-template<class T, class Comp>
-void BinaryHeap<T,Comp>::decrease(std::size_t i, const T& value) {
+void BinaryHeap<T,Comp>::decrease(std::size_t i, const int value) {
       if (compare(data[i], value)) throw std::invalid_argument("value is not smaller than H[i]");
-      data[i] = value;
+      data[i].d = value;
       bubble_up(i);
 }
 
-template<class T, class Comp>
-void BinaryHeap<T,Comp>::insert(const T& value) {
-    check_and_alloc();
-    data[size] = value;
-    bubble_up(size);
-    ++size;
-    --free_slots;
-}
+#endif // __HEAP__
